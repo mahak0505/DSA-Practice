b502e98429396d2edb62854f3b9e7fc77f5ae9f6
mahak0505 <mahakb.556@gmail.com>
2025-07-06 17:07:30 +0530

    Selection Sort

---
+++ Arrays/selectionSort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void selectionSort(vector<int>& arr,int n){
+    for ( int i = 0; i < n-1; i++)
+    {
+         int minIndex=i;
+         for ( int j = i+1; j < n; j++)
+         {
+             if(arr[minIndex]>arr[j]){
+                minIndex=j;
+             }
+         }
+         swap(arr[minIndex],arr[i]);
+         
+    }
+    
+}
+
+void printArray(vector<int>& arr,int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main (){
+     vector<int> arr = {10, 22, 5, -1, 30, 84, 9};
+     selectionSort(arr,7);
+     printArray(arr,7);
+} 
