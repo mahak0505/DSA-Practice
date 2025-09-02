06f0af797e3ffb9537a2942b880c83d031e19990
mahak0505 <mahakb.556@gmail.com>
2025-09-02 12:20:56 +0530

    recursion day 1

---
+++ Recursion/print.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void printCounting(int n){
+    if(n==0){
+    return;
+    }
+    cout<<n<<" ";
+    printCounting(n-1);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    printCounting(n);
+    return 0;
+}
